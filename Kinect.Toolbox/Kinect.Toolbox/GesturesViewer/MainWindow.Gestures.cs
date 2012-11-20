7164d1a3e7da27326119b4340000eb4a41d4b9d1
Harish Vaidyanathan <harishsrinivas89@gmail.com>
2012-11-19 22:46:56 -0800

    updated kinect code

---
+++ Kinect.Toolbox/Kinect.Toolbox/GesturesViewer/MainWindow.Gestures.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using Kinect.Toolbox;
+
+namespace GesturesViewer
+{
+    partial class MainWindow
+    {
+        void LoadCircleGestureDetector()
+        {
+            Stream recordStream = File.Open(circleKBPath, FileMode.OpenOrCreate);
+            circleGestureRecognizer = new TemplatedGestureDetector("Circle", recordStream);
+            circleGestureRecognizer.TraceTo(gesturesCanvas, Colors.Red);
+            circleGestureRecognizer.OnGestureDetected += OnGestureDetected;
+
+            templates.ItemsSource = circleGestureRecognizer.LearningMachine.Paths;
+        }
+
+        private void recordCircle_Click(object sender, RoutedEventArgs e)
+        {
+            if (circleGestureRecognizer.IsRecordingPath)
+            {
+                circleGestureRecognizer.EndRecordTemplate();
+                recordCircle.Content = "Record new Circle";
+                return;
+            }
+
+            circleGestureRecognizer.StartRecordTemplate();
+            recordCircle.Content = "Stop Recording";
+        }
+
+        void OnGestureDetected(string gesture)
+        {
+            int pos = detectedGestures.Items.Add(string.Format("{0} : {1}", gesture, DateTime.Now));
+
+            detectedGestures.SelectedIndex = pos;
+        }
+
+        void CloseGestureDetector()
+        {
+            Stream recordStream = File.Create(circleKBPath);
+            circleGestureRecognizer.SaveState(recordStream);
+        }
+    }
+}
